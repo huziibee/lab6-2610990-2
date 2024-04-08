f4a1df6f19b4bfaef6f4d6c12744adcc11e08d76
huziibee <mhbangie@gmail.com>
2024-04-08 21:25:30 +0200

    deployed

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+//
 const cars = require('./cars.json');
 
 // get all cars
