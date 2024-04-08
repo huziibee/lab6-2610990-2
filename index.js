2f6eca004f4488f6920073c43a68d2843d730b25
huziibee <mhbangie@gmail.com>
2024-04-08 21:13:42 +0200

    deployed

--- index.js
+++ index.js
@@ -1,11 +1,62 @@
-const express = require("express");
+//create cars api using express
+const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const path = require('path');
+// const cors = require('cors');
 
-app.get("/", (req, res) => {
-  res.send("Hello, Worlds!");
+app.use(express.json());
+// app.use(cors());
+const cars = require('./cars.json');
+
+// get all cars
+app.get('/cars', (req, res) => {
+    res.json(cars);
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+//
+
+//get car by id
+app.get('/cars/:id', (req, res) => {
+    const id = req.params.id;
+    const car = cars.find(car => car.id === id);
+    res.json(car);
+});
+
+//update car
+app.put('/cars/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedCar = req.body;
+    const index = cars.findIndex(car => car.id === id);
+    cars[index] = updatedCar;
+    res.json(updatedCar);
+});
+
+//delete car
+app.delete('/cars/:id', (req, res) => {
+    const id = req.params.id;
+    const index = cars.findIndex(car => car.id === id);
+    cars.splice(index, 1);
+    res.json({ message: `Car with id ${id} deleted` });
+});
+
+//add car
+app.post('/cars', (req, res) => {
+    console.log(req);
+    const newCar = req.body;
+    console.log(newCar);
+    cars.push(newCar);
+    res.json(newCar);
+});
+
+// app.use(express.static(path.resolve(__dirname, './'), { maxAge : '1y', etag: false}));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'));
+  });
+
+  //
+
+//start app at localhost:3001
+app.listen(process.env.PORT || 3001, () => {
+    console.log('Server started at http://localhost:3001');
 });
